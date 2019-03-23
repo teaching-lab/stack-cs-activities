ba8eec6922c7249b3859c62cf106d90ac954277b
Stefan Bojnak <xbojnak@fi.muni.cz>
2019-03-23 21:32:43 +0100

    Added test illustration to TDD activity

---
+++ activities/test-driven-development/FactorialProject/FactorialProject/FactorialClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProject
+{
+    public static class FactorialClass
+    {
+        public static int ComputeFactorial(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
